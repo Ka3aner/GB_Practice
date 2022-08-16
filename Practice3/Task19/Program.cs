31ffcb3f6e79690fc9287421d3836de6feec4300
Said Yarullin <said.98@mail.ru>
2022-08-16 13:25:18 +0300

    Task19 added

---
+++ Practice3/Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+
+Console.WriteLine("Введите пятизначное число для проверки");
+
+string str = Console.ReadLine();
+string strabs = Math.Abs(int.Parse(str)).ToString();
+string answer = "Данное число является палиндромом";
+
+for(int i = 0; i < strabs.Length; i++)
+{
+    if (strabs[i] != strabs[strabs.Length-1-i])
+    {
+        answer = "Данное число НЕ является палиндромом";
+        break;
+    }
+}
+
+Console.WriteLine(answer);
---
+++ Practice3/Task21/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetInt(string message)
+{
+    Console.Write(message+": ");
+    string str = Console.ReadLine();
+    int number;
+    if (int.TryParse(str, out number)) return number;
+    else
+    {
+        Console.WriteLine("Введено не целое число, повторите ввод");
+        GetInt(message);
+    }
+    return 0;
+}
