ae5cb8118069326ab839183bb0fb4e146b5f0a2c
Said Yarullin <said.98@mail.ru>
2022-09-01 18:54:32 +0300

    Task 64 added

---
+++ Practice9/Task64/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+int GetInt(string message)
+{
+    Console.Write(message+": ");
+    string str = Console.ReadLine();
+    if (!int.TryParse(str, out int number))
+    {
+        Console.WriteLine("Введено не целое число, повторите ввод");
+        return GetInt(message);
+    }
+    return number;
+}
+
+int m = GetInt("Введите первое число ряда");
+int n = GetInt("Введите последнее число ряда");
+
+void IntBetweenMAndN(int m, int n)
+{
+    if (m < n)
+    {
+        if (m == n)
+        {
+            Console.Write(m);
+            return;
+        }
+        Console.Write(m+", ");
+        IntBetweenMAndN(m + 1, n);
+    }
+    else
+    {
+        if (m == n)
+        {
+            Console.Write(m + "]");
+            return;
+        }
+        Console.Write(m + ", ");
+        IntBetweenMAndN(m - 1, n);
+    }
+}
+Console.Write("Получившийся ряд: [");
+IntBetweenMAndN(m, n);
